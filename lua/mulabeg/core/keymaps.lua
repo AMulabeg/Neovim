f099e7800ea56d2089ad1083a0b44dba41db7539
Amer Mulabegovic <amermulabegovic@gmail.com>
2023-12-10 18:13:32 +0100

    added built in terminal and other small fixes

--- lua/mulabeg/core/keymaps.lua
+++ lua/mulabeg/core/keymaps.lua
@@ -30,3 +30,6 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+keymap.set("n", "<leader>jh", "<cmd>ToggleTerm direction=horizontal<cr>", { desc = "Open Terminal Horizontally" })
+keymap.set("n", "<leader>jt", "<cmd>ToggleTerm direction=tab<cr>", { desc = "Open Terminal Tab" })
