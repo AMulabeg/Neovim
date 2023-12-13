3f0655941bfbbd6dba0db672abc5236d9043e988
Amer Mulabegovic <amermulabegovic@gmail.com>
2023-12-13 10:21:19 +0100

    I can't spell :(

--- lua/mulabeg/plugins/presence.lua
+++ lua/mulabeg/plugins/presence.lua
@@ -3,12 +3,12 @@ return {
   event = "BufEnter",
   config = function()
     require("presence"):setup({
-      neovim_image_text = "Edditing image",
+      neovim_image_text = "Editing image",
       debounce_timeout = 42,
-      editing_text = "Edditing %s",
+      editing_text = "Editing %s",
       file_explorer_text = "file explorer %s",
       git_commit_text = "Git ",
-      plugin_manager_text = "Edditing pluggins",
+      plugin_manager_text = "Editing plugins",
       reading_text = "Reading %s",
       workspace_text = "Workspace  %s",
       line_number_text = "Line number %s",
