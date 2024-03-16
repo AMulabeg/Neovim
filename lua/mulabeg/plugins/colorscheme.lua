daf3bbf0c89f4fbf67368027d4cbb5b41482da41
Amer Mulabegovic <amermulabegovic@gmail.com>
2024-03-16 19:25:00 +0100

    colorscheme change

--- lua/mulabeg/plugins/colorscheme.lua
+++ lua/mulabeg/plugins/colorscheme.lua
@@ -1,11 +1,12 @@
 return {
   {
-    "oxfist/night-owl.nvim",
+    --"oxfist/night-owl.nvim",
+    "folke/tokyonight.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      -- load the colorscheme here
-      vim.cmd.colorscheme("night-owl")
+      --load the colorscheme here
+      vim.cmd.colorscheme("tokyonight-night")
     end,
   },
 }
