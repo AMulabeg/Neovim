0f8f93b74acc45332f5d4c77236ba12064e3903c
Amer Mulabegovic <amermulabegovic@gmail.com>
2024-04-02 18:10:11 +0200

    Changing lualine (Ty Redyf)

--- lua/mulabeg/plugins/lualine.lua
+++ lua/mulabeg/plugins/lualine.lua
@@ -52,6 +52,8 @@ return {
     lualine.setup({
       options = {
         theme = my_lualine_theme,
+        component_separators = { left = "|", right = "|" },
+        section_separators = { left = "█", right = "█" },
       },
       sections = {
         lualine_x = {
