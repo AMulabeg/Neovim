d9893cbe81819f0c80070d37cf23cfcbc96e7bd3
Amer Mulabegovic <amermulabegovic@gmail.com>
2024-05-01 17:05:29 +0200

    Switching out my outdated mason/lsp config

--- lua/mulabeg/plugins/lsp/mason.lua
+++ lua/mulabeg/plugins/lsp/mason.lua
@@ -32,18 +32,18 @@ return {
         "clangd",
         "kotlin_language_server",
         "gopls",
-        "marksman",
+        "ruff",
       },
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true, -- not the same as ensure_installed
     })
 
     mason_tool_installer.setup({
       ensure_installed = {
+        "prettier", -- prettier formatter
         "stylua", -- lua formatter
         "isort", -- python formatter
         "black", -- python formatter
-        "pylint", -- python linter
+        -- "pylint",
+        "mypy",
         "clang-format",
         "ktlint",
       },
