8f2a9a6c1252da4a310b6aa365f3cdbc02ac2a2d
Amer Mulabegovic <amermulabegovic@gmail.com>
2024-06-12 21:22:16 +0200

    Removing some stuff

--- lua/mulabeg/plugins/oil-nvim.lua
+++ lua/mulabeg/plugins/oil-nvim.lua
@@ -2,8 +2,23 @@ return {
   {
     "stevearc/oil.nvim",
     opts = {},
-    -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
+    config = function()
+      require("oil").setup({
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+          show_hidden = true,
+          natural_order = true,
+          is_always_hidden = function(name, _)
+            return name == ".." or name == ".git"
+          end,
+        },
+        win_options = {
+          wrap = true,
+        },
+      })
+    end,
   },
 }
