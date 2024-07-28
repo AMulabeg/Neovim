4ab1ed4c3188acea90951016e42985a8086701e9
Amer Mulabegovic <amermulabegovic@gmail.com>
2024-07-28 21:20:43 +0200

    Small bug fix

---
+++ lua/mulabeg/plugins/vim-tmux.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    event = "VeryLazy",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+  },
+}
