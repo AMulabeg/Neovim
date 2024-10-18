8351cecb56e2dca3ce3b8c9ee147aa62178da9e8
Amer Mulabegovic <amermulabegovic@gmail.com>
2024-10-18 12:27:08 +0200

    silly changes

--- lua/mulabeg/plugins/lualine.lua
+++ lua/mulabeg/plugins/lualine.lua
@@ -52,7 +52,7 @@ return {
     -- configure lualine with modified theme
     lualine.setup({
       options = {
-        theme = "TokyoNight",
+        theme = my_lualine_theme,
         component_separators = { left = "|", right = "|" },
         section_separators = { left = "█", right = "█" },
       },
