dabac7b02a2d55326dc6220730c0912bba51c961
Amer Mulabegovic <amermulabegovic@gmail.com>
2024-11-04 16:29:28 +0100

    testing something

---
+++ lua/mulabeg/plugins/cmp.lua
@@ -0,0 +1,56 @@
+return {
+  "saghen/blink.cmp",
+  lazy = false, -- lazy loading handled internally
+  -- optional: provides snippets for the snippet source
+  dependencies = "rafamadriz/friendly-snippets",
+
+  -- use a release tag to download pre-built binaries
+  version = "v0.*",
+  -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source using latest nightly rust with:
+  -- build = 'nix run .#build-plugin',
+
+  ---@module 'blink.cmp'
+  opts = {
+    -- 'default' for mappings similar to built-in completion
+    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+    -- see the "default configuration" section below for full documentation on how to define
+    -- your own keymap.
+    keymap = { preset = "default" },
+
+    highlight = {
+      -- sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- useful for when your theme doesn't support blink.cmp
+      -- will be removed in a future release, assuming themes add support
+      use_nvim_cmp_as_default = true,
+    },
+    -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+    -- adjusts spacing to ensure icons are aligned
+    nerd_font_variant = "mono",
+
+    -- experimental auto-brackets support
+    -- accept = { auto_brackets = { enabled = true } }
+
+    -- experimental signature help support
+    -- trigger = { signature_help = { enabled = true } }
+  },
+},
+-- LSP servers and clients communicate what features they support through "capabilities".
+--  By default, Neovim support a subset of the LSP specification.
+--  With blink.cmp, Neovim has *more* capabilities which are communicated to the LSP servers.
+--  Explanation from TJ: https://youtu.be/m8C0Cq9Uv9o?t=1275
+--
+-- This can vary by config, but in-general for nvim-lspconfig:
+{
+  "neovim/nvim-lspconfig",
+  dependencies = { "saghen/blink.cmp" },
+  config = function(_, opts)
+    local lspconfig = require("lspconfig")
+    for server, config in pairs(opts.servers) do
+      config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+      lspconfig[server].setup(config)
+    end
+  end,
+}
---
+++ lua/mulabeg/plugins/transparent-nvim.lua
@@ -0,0 +1,4 @@
+return {
+  lazy = false,
+  "xiyaowong/transparent.nvim",
+}
