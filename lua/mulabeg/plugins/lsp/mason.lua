79eaebc0963307d6d7048de41e041648ae3fa8b9
Amer Mulabeg <a.mulabeg@proton.me>
2025-05-06 11:26:18 +0200

    SOOOOOOOOOOOOOO I made a massive oopsie. Not quite sure if I reverted the correct thing, but it is what it is. Going to use this for the near future until all the blink stuff is sorted out

--- lua/mulabeg/plugins/lsp/mason.lua
+++ lua/mulabeg/plugins/lsp/mason.lua
@@ -5,7 +5,7 @@ return {
       "williamboman/mason-lspconfig.nvim",
       "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
-    lazy = false,
+    event = "VeryLazy",
     config = function()
       require("mason").setup({
         ui = {
