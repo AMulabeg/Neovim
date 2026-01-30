4c609db2030dd762847ce2dee221810c8234425c
Amer Mulabegovic <a.mulabeg@proton.me>
2026-01-30 10:20:15 +0100

    treesitter broke

---
+++ lua/mulabeg/plugins/treesitter-context.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "VeryLazy",
+    enabled = false,
+    config = function()
+      require("treesitter-context").setup({
+        mode = "topline",
+      })
+    end,
+  },
+}
--- lua/mulabeg/plugins/treesitter.lua
+++ lua/mulabeg/plugins/treesitter.lua
@@ -1,73 +1,15 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "master",
     build = ":TSUpdate",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
+    lazy = false,
     config = function()
       local configs = require("nvim-treesitter.configs")
-
       configs.setup({
+        auto_install = true,
         highlight = {
           enable = true,
-        },
-        indent = {
-          enable = true,
-        },
-        autopairs = {
-          enable = true,
-        },
-        folding = {
-          enable = true,
-        },
-        preview = {
-          icon_provider = "mini",
-        },
-
-        auto_install = true,
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<c-backspace>",
-          },
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["]m"] = "@function.outer",
-              ["]]"] = "@class.outer",
-            },
-            goto_next_end = {
-              ["]M"] = "@function.outer",
-              ["]["] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[m"] = "@function.outer",
-              ["[["] = "@class.outer",
-            },
-            goto_previous_end = {
-              ["[M"] = "@function.outer",
-              ["[]"] = "@class.outer",
-            },
-          },
         },
       })
     end,
